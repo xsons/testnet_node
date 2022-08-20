1a72615a1d89cd800edee554a55d9f286ed4f856
xsons <108946833+xsons@users.noreply.github.com>
2022-08-20 19:50:12 +0700

    Update validator.sh

--- Aptos/validator.sh
+++ Aptos/validator.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+clear
+echo "=================================================="  
 echo -e "\033[0;35m"
 echo " __  ______   ___  _   _ ____  ";
 echo " \ \/ / ___| / _ \| \ | / ___| ";
@@ -6,108 +8,7 @@ echo "  \  /\___ \| | | |  \| \___ \ ";
 echo "  /  \ ___) | |_| | |\  |___) |";
 echo " /_/\_\____/ \___/|_| \_|____/ ";
 echo -e "\e[0m"
+echo "=================================================="  
 
 sleep 2
 
-# set vars
-if [ ! $NODENAME ]; then
-	read -p "Enter node name: " NODENAME
-	echo 'export NODENAME='$NODENAME >> $HOME/.bash_profile
-fi
-echo "export WORKSPACE=testnet" >> $HOME/.bash_profile
-echo "export PUBLIC_IP=$(curl -s ifconfig.me)" >> $HOME/.bash_profile
-source $HOME/.bash_profile
-
-echo -e "\e[1m\e[32m1. Updating dependencies... \e[0m" && sleep 1
-sudo apt update && sudo apt upgrade -y
-
-echo -e "\e[1m\e[32m2. Installing required dependencies... \e[0m" && sleep 1
-sudo apt-get install jq unzip -y
-sudo wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/download/v4.23.1/yq_linux_amd64 && chmod +x /usr/local/bin/yq
-
-echo -e "\e[1m\e[32m3. Checking if Docker is installed... \e[0m" && sleep 1
-if ! command -v docker &> /dev/null
-then
-    echo -e "\e[1m\e[32m3.1 Installing Docker... \e[0m" && sleep 1
-    sudo apt-get install ca-certificates curl gnupg lsb-release -y
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt-get update
-    sudo apt-get install docker-ce docker-ce-cli containerd.io -y
-fi
-
-echo -e "\e[1m\e[32m4. Checking if Docker Compose is installed ... \e[0m" && sleep 1
-docker-compose version
-if [ $? -ne 0 ]
-then
-    echo -e "\e[1m\e[32m4.1 Installing Docker Compose... \e[0m" && sleep 1
-	docker_compose_version=$(wget -qO- https://api.github.com/repos/docker/compose/releases/latest | jq -r ".tag_name")
-	sudo wget -O /usr/bin/docker-compose "https://github.com/docker/compose/releases/download/${docker_compose_version}/docker-compose-`uname -s`-`uname -m`"
-	sudo chmod +x /usr/bin/docker-compose
-fi
-
-# download aptos-cli
-wget -qO aptos-cli.zip https://github.com/aptos-labs/aptos-core/releases/download/aptos-cli-v0.3.1/aptos-cli-0.3.1-Ubuntu-x86_64.zip
-sudo unzip -o aptos-cli.zip -d /usr/local/bin
-chmod +x /usr/local/bin/aptos
-rm aptos-cli.zip
-
-echo -e "\e[1m\e[32m5. Installing Validator Node ... \e[0m" && sleep 1
-mkdir ~/$WORKSPACE && cd ~/$WORKSPACE
-
-# download configs
-wget -qO docker-compose.yaml https://raw.githubusercontent.com/aptos-labs/aptos-core/main/docker/compose/aptos-node/docker-compose.yaml
-wget -qO validator.yaml https://raw.githubusercontent.com/aptos-labs/aptos-core/main/docker/compose/aptos-node/validator.yaml
-
-# generate keys
-aptos genesis generate-keys --output-dir ~/$WORKSPACE/keys
-
-# configure validator
-aptos genesis set-validator-configuration \
-    --local-repository-dir ~/$WORKSPACE \
-    --username $NODENAME \
-    --owner-public-identity-file ~/$WORKSPACE/keys/public-keys.yaml \
-    --validator-host $PUBLIC_IP:6180 \
-    --stake-amount 100000000000000
-  
-# add layout file
-sudo tee layout.yaml > /dev/null <<EOF
----
-root_key: "D04470F43AB6AEAA4EB616B72128881EEF77346F2075FFE68E14BA7DEBD8095E"
-users:
-  - $NODENAME
-chain_id: 43
-allow_new_validators: false
-epoch_duration_secs: 7200
-is_test: true
-min_stake: 100000000000000
-min_voting_threshold: 100000000000000
-max_stake: 100000000000000000
-recurring_lockup_duration_secs: 86400
-required_proposer_stake: 100000000000000
-rewards_apy_percentage: 10
-voting_duration_secs: 43200
-voting_power_increase_limit: 20
-EOF
-
-# download aptos framework
-wget -qO framework.mrb https://github.com/aptos-labs/aptos-core/releases/download/aptos-framework-v0.3.0/framework.mrb -P ~/$WORKSPACE
-
-# compile genesis blob and waypoint
-aptos genesis generate-genesis --local-repository-dir ~/$WORKSPACE --output-dir ~/$WORKSPACE
-
-# run docker compose
-docker-compose up -d
-
-
-echo -e "\e[1m\e[32mAptos Validator Node Started \e[0m"
-echo -e "Please backup key files located in \e[1m\e[32m~/$WORKSPACE/keys\e[0m and \e[1m\e[32m~/$WORKSPACE/$NODENAME \e[0m"
-
-echo -e "\e[1m\e[32mVerify initial synchronization: \e[0m" 
-echo -e "\e[1m\e[39m    curl 127.0.0.1:9101/metrics 2> /dev/null | grep aptos_state_sync_version | grep type \n \e[0m" 
-
-echo -e "\e[1m\e[32mTo view validator node logs: \e[0m" 
-echo -e "\e[1m\e[39m    docker logs -f testnet-validator-1 --tail 50 \n \e[0m" 
-
-echo -e "\e[1m\e[32mTo view keys: \e[0m" 
-echo -e "\e[1m\e[39m    cat ~/$WORKSPACE/keys/public-keys.yaml \n \e[0m" 
