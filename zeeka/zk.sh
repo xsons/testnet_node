c30622a68177b762a894f5f69c76b34a0f094d21
xsons <108946833+xsons@users.noreply.github.com>
2022-08-30 20:37:48 +0700

    Update zk.sh

--- zeeka/zk.sh
+++ zeeka/zk.sh
@@ -10,19 +10,6 @@ echo -e "\e[0m"
 
 sleep 2
 
-# set vars
-if [ ! $PHARSE ]; then
-	read -p "YOUR PHARSE: " PHARSE
-	echo 'export PHARSE='$PHARSE >> $HOME/.bash_profile
-fi
-if [ ! $IP ]; then
-	read -p "YOUR IP VPS: " IP
-	echo 'export IP='$IP >> $HOME/.bash_profile
-fi
-source $HOME/.bash_profile
-
-sleep 2
-
 echo -e "\e[1m\e[32m1. Updating packages... \e[0m" && sleep 1
 # update
 sudo apt update && sudo apt upgrade -y
@@ -43,9 +30,4 @@ git clone https://github.com/zeeka-network/bazuka && cd bazuka
 git pull origin master
 cargo install --path .
 
-echo -e "\e[1m\e[32m5. Installing Bazuka... \e[0m" && sleep 1
-# Init
-bazuka init --seed '$PHARSE' --network debug --node $IP:8765
-
-bazuka node --listen 0.0.0.0:8765 --external $IP:8765 \
-  --network debug --db ~/.bazuka-debug --bootstrap 152.228.155.120:8765 --bootstrap 95.182.120.179:8765 --bootstrap 195.2.80.120:8765 --bootstrap 195.54.41.148:8765 --bootstrap 65.108.244.233:8765 --bootstrap 195.54.41.130:8765 --bootstrap 185.213.25.229:8765 --bootstrap 195.54.41.115:8765 --bootstrap 62.171.188.69:8765 --bootstrap 49.12.229.140:8765 --bootstrap 213.202.238.77:8765 --bootstrap 5.161.152.123:8765 --bootstrap 65.108.146.132:8765 --bootstrap 65.108.250.158:8765 --bootstrap 195.2.73.130:8765 --bootstrap 188.34.167.3:8765 --bootstrap 188.34.166.77:8765 --bootstrap 45.88.106.199:8765 --bootstrap 79.143.188.183:8765 --bootstrap 62.171.171.11:8765 --bootstrap 65.108.201.41:8765 --bootstrap 159.203.176.252:8765 --bootstrap 194.163.191.80:8765 --bootstrap 146.19.207.4:8765 --bootstrap 135.181.43.174:8765 --bootstrap 95.111.234.205:8765 --bootstrap 192.241.131.113:8765 --bootstrap 45.67.217.16:8765 --bootstrap 65.108.157.67:8765 --bootstrap 65.108.251.175:8765 --bootstrap 95.216.204.235:8765 --bootstrap 45.82.178.159:8765 --bootstrap 161.97.111.145:8765 --bootstrap 149.102.133.130:8765 --bootstrap 65.108.61.32:8765 --bootstrap 95.216.204.32:8765 --bootstrap 188.34.160.74:8765 --bootstrap 185.245.183.246:8765 --bootstrap 213.246.39.14:8765
+echo '=============== SETUP FINISHED ==================='
