473a37397cd025cad2963cac1223a9bee4d8b448
xsons <108946833+xsons@users.noreply.github.com>
2022-09-07 00:53:44 +0700

    Update source.sh

--- Source/source.sh
+++ Source/source.sh
@@ -5,7 +5,15 @@ do
 
 # Logo
 
-curl -s https://raw.githubusercontent.com/xsons/TestnetNode/main/logoxsons | bash
+#!/bin/bash
+
+echo -e "\033[0;35m"
+echo "  __  ______   ___  _   _ ____   ";
+echo "  \ \/ / ___| / _ \| \ | / ___|  ";
+echo "   \  /\___ \| | | |  \| \___ \  ";
+echo "   /  \ ___) | |_| | |\  |___) | ";
+echo "  /_/\_\____/ \___/|_| \_|____/  ";
+echo -e "\e[0m"
 
 # Menu
 
