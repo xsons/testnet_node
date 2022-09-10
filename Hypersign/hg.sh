16d07d4f05e5a9a1b434dea0e47313bbfa1db59e
xsons <108946833+xsons@users.noreply.github.com>
2022-09-10 12:10:37 +0700

    Update hg.sh

--- Hypersign/hg.sh
+++ Hypersign/hg.sh
@@ -1,1 +1,33 @@
+#!/bin/bash
 
+echo -e "\033[0;35m"
+echo "  __  ______   ___  _   _ ____   ";
+echo "  \ \/ / ___| / _ \| \ | / ___|  ";
+echo "   \  /\___ \| | | |  \| \___ \  ";
+echo "   /  \ ___) | |_| | |\  |___) | ";
+echo "  /_/\_\____/ \___/|_| \_|____/  ";
+echo -e "\e[0m"
+
+sleep 2
+
+echo -e "\e[1m\e[32m1. Updating packages... \e[0m" && sleep 1
+# update
+sudo apt update && sudo apt upgrade -y
+
+echo -e "\e[1m\e[32m2. Installing dependencies... \e[0m" && sleep 1
+# packages
+sudo apt install curl build-essential git wget jq make gcc tmux -y
+
+# install go
+ver="1.18.2"
+cd $HOME
+wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
+rm "go$ver.linux-amd64.tar.gz"
+echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile
+source ~/.bash_profile
+go version
+
+echo -e "\e[1m\e[32m3. Downloading and building binaries... \e[0m" && sleep 1
+# download binary
