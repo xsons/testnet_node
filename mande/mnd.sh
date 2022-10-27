5f8a9079a3626cf0a2ee99e72cadd6954c1fb46a
xsons <108946833+xsons@users.noreply.github.com>
2022-10-27 18:27:57 +0700

    Update mnd.sh

--- mande/mnd.sh
+++ mande/mnd.sh
@@ -1,107 +1,120 @@
 #!/bin/bash
+echo -e "\033[0;35m"
+echo " ########   ######           ######      ###    ##    ##  ######   "; 
+echo " ##     ## ##    ##         ##    ##    ## ##   ###   ## ##    ##  ";
+echo " ##     ## ##               ##         ##   ##  ####  ## ##        ";
+echo " ########  ##       ####### ##   #### ##     ## ## ## ## ##   #### ";
+echo " ##     ## ##               ##    ##  ######### ##  #### ##    ##  ";
+echo " ##     ## ##    ##         ##    ##  ##     ## ##   ### ##    ##  ";
+echo " ########   ######           ######   ##     ## ##    ##  ######   ";
+echo -e "\e[0m"
 
-while true
-do
 
-# Logo
+sleep 2
 
-curl -s https://raw.githubusercontent.com/xsons/testnet_node/main/logoxsons | bash
+# set vars
+if [ ! $NODENAME ]; then
+	read -p "Enter node name: " NODENAME
+	echo 'export NODENAME='$NODENAME >> $HOME/.bash_profile
+fi
+MANDE_PORT=38
+if [ ! $WALLET ]; then
+	echo "export WALLET=wallet" >> $HOME/.bash_profile
+fi
+echo "export MANDE_CHAIN_ID=mande-testnet-1" >> $HOME/.bash_profile
+echo "export MANDE_PORT=${MANDE_PORT}" >> $HOME/.bash_profile
+source $HOME/.bash_profile
 
-# Menu
+echo '================================================='
+echo -e "Your node name: \e[1m\e[32m$NODENAME\e[0m"
+echo -e "Your wallet name: \e[1m\e[32m$WALLET\e[0m"
+echo -e "Your chain name: \e[1m\e[32m$MANDE_CHAIN_ID\e[0m"
+echo -e "Your port: \e[1m\e[32m$MANDE_PORT\e[0m"
+echo '================================================='
+sleep 2
 
-PS3='Select an action: '
-options=(
-"Install Node"
-"Create wallet"
-"Check node logs"
-"Synchronization via StateSync"
-"Synchronization via SnapShot"
-"UPDATE"
-"Delete Node"
-"Exit")
-select opt in "${options[@]}"
-do
-case $opt in
-
-"Install Node")
-echo "*********************"
-echo -e "\e[1m\e[35m		Lets's begin\e[0m"
-echo "*********************"
-echo -e "\e[1m\e[32m	Enter your Validator_Name:\e[0m"
-echo "_|-_|-_|-_|-_|-_|-_|"
-read Validator_Name
-echo "_|-_|-_|-_|-_|-_|-_|"
-echo export Validator_Name=${Validator_Name} >> $HOME/.bash_profile
-echo export CHAIN_ID="mande-testnet-1" >> $HOME/.bash_profile
-source ~/.bash_profile
-
-echo -e "\e[1m\e[32m1. Updating packages and dependencies--> \e[0m" && sleep 1
-#UPDATE APT
+echo -e "\e[1m\e[32m1. Updating packages... \e[0m" && sleep 1
+# update
 sudo apt update && sudo apt upgrade -y
-sudo apt install curl tar wget clang pkg-config libssl-dev libleveldb-dev jq build-essential bsdmainutils git make ncdu htop screen unzip bc fail2ban htop -y
 
-echo -e "        \e[1m\e[32m2. Installing GO--> \e[0m" && sleep 1
-#INSTALL GO
-wget https://golang.org/dl/go1.18.1.linux-amd64.tar.gz; \
-rm -rv /usr/local/go; \
-tar -C /usr/local -xzf go1.18.1.linux-amd64.tar.gz && \
-rm -v go1.18.1.linux-amd64.tar.gz && \
-echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile && \
-source ~/.bash_profile && \
-go version
+echo -e "\e[1m\e[32m2. Installing dependencies... \e[0m" && sleep 1
+# packages
+sudo apt install curl build-essential git wget jq make gcc tmux chrony -y
 
-echo -e "              \e[1m\e[32m3. Downloading and building binaries--> \e[0m" && sleep 1
-#INSTALL
+# install go
+if ! [ -x "$(command -v go)" ]; then
+  ver="1.18.2"
+  cd $HOME
+  wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
+  sudo rm -rf /usr/local/go
+  sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
+  rm "go$ver.linux-amd64.tar.gz"
+  echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile
+  source ~/.bash_profile
+fi
+
+echo -e "\e[1m\e[32m3. Downloading and building binaries... \e[0m" && sleep 1
+# download binary
 cd $HOME
 curl -OL https://github.com/mande-labs/testnet-1/raw/main/mande-chaind
 mkdir -p $HOME/go/bin
 mv mande-chaind /$HOME/go/bin/
 chmod 744 /$HOME/go/bin/mande-chaind
 
-mande-chaind init $Validator_Name --chain-id $CHAIN_ID
+# config
+mande-chaind config chain-id $MANDE_CHAIN_ID
+mande-chaind config keyring-backend test
+mande-chaind config node tcp://localhost:${MANDE_PORT}657
 
-cd $HOME
-wget -O $HOME/.mande-chain/config/genesis.json "https://raw.githubusercontent.com/mande-labs/testnet-1/main/genesis.json"
-wget -O $HOME/.mande-chain/config/addrbook.json "https://raw.githubusercontent.com/obajay/nodes-Guides/main/Mande%20Chain/addrbook.json"
+# init
+mande-chaind init $NODENAME --chain-id $MANDE_CHAIN_ID
 
+# download genesis and addrbook
+wget -qO $HOME/.mande-chain/config/genesis.json "https://raw.githubusercontent.com/mande-labs/testnet-1/main/genesis.json"
+wget -qO $HOME/.mande-chain/config/addrbook.json "https://raw.githubusercontent.com/obajay/nodes-Guides/main/Mande%20Chain/addrbook.json"
 
-echo -e "                     \e[1m\e[32m4. Node optimization and improvement--> \e[0m" && sleep 1
+# set peers and seeds
+SEEDS="cd3e4f5b7f5680bbd86a96b38bc122aa46668399@34.171.132.212:26656"
+PEERS="6780b2648bd2eb6adca2ca92a03a25b216d4f36b@34.170.16.69:26656,a3e3e20528604b26b792055be84e3fd4de70533b@38.242.199.93:24656"
+sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.mande-chain/config/config.toml
 
-sed -i.bak -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0.0mand\"/;" ~/.mande-chain/config/app.toml
-sed -i -e "s/^filter_peers *=.*/filter_peers = \"true\"/" $HOME/.mande-chain/config/config.toml
-external_address=$(wget -qO- eth0.me) 
-sed -i.bak -e "s/^external_address *=.*/external_address = \"$external_address:26656\"/" $HOME/.mande-chain/config/config.toml
-peers="cd3e4f5b7f5680bbd86a96b38bc122aa46668399@34.171.132.212:26656,6780b2648bd2eb6adca2ca92a03a25b216d4f36b@34.170.16.69:26656,a3e3e20528604b26b792055be84e3fd4de70533b@38.242.199.93:24656"
-sed -i.bak -e "s/^persistent_peers *=.*/persistent_peers = \"$peers\"/" $HOME/.mande-chain/config/config.toml
-seeds="cd3e4f5b7f5680bbd86a96b38bc122aa46668399@34.171.132.212:26656"
-sed -i.bak -e "s/^seeds =.*/seeds = \"$seeds\"/" $HOME/.mande-chain/config/config.toml
-sed -i 's/max_num_inbound_peers =.*/max_num_inbound_peers = 100/g' $HOME/.mande-chain/config/config.toml
-sed -i 's/max_num_outbound_peers =.*/max_num_outbound_peers = 100/g' $HOME/.mande-chain/config/config.toml
+# set custom ports
+sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:${MANDE_PORT}658\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:${MANDE_PORT}657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:${MANDE_PORT}060\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:${MANDE_PORT}656\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":${MANDE_PORT}660\"%" $HOME/.mande-chain/config/config.toml
+sed -i.bak -e "s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:${MANDE_PORT}317\"%; s%^address = \":8080\"%address = \":${MANDE_PORT}080\"%; s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:${MANDE_PORT}090\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:${MANDE_PORT}091\"%; s%^address = \"0.0.0.0:8545\"%address = \"0.0.0.0:${MANDE_PORT}545\"%; s%^ws-address = \"0.0.0.0:8546\"%ws-address = \"0.0.0.0:${MANDE_PORT}546\"%" $HOME/.mande-chain/config/app.toml
 
+# config pruning
+pruning="custom"
+pruning_keep_recent="100"
+pruning_keep_every="0"
+pruning_interval="50"
+sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" $HOME/.mande-chain/config/app.toml
+sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" $HOME/.mande-chain/config/app.toml
+sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" $HOME/.mande-chain/config/app.toml
+sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/.mande-chain/config/app.toml
 
-# pruning and indexer
-pruning="custom" && \
-pruning_keep_recent="100" && \
-pruning_keep_every="0" && \
-pruning_interval="10" && \
-sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" ~/.mande-chain/config/app.toml && \
-sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" ~/.mande-chain/config/app.toml && \
-sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" ~/.mande-chain/config/app.toml && \
-sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" ~/.mande-chain/config/app.toml
-indexer="null" && \
-sed -i -e "s/^indexer *=.*/indexer = \"$indexer\"/" $HOME/.mande-chain/config/config.toml
+# set minimum gas price and timeout commit
+sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0mand\"/" $HOME/.mande-chain/config/app.toml
 
+# enable prometheus
+sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.mande-chain/config/config.toml
 
+# reset
+mande-chaind tendermint unsafe-reset-all --home $HOME/.mande-chain
+
+echo -e "\e[1m\e[32m4. Starting service... \e[0m" && sleep 1
+# create service
 sudo tee /etc/systemd/system/mande-chaind.service > /dev/null <<EOF
 [Unit]
-Description=mande-chaind
+Description=mande
 After=network-online.target
+
 [Service]
 User=$USER
-ExecStart=$(which mande-chaind) start
+ExecStart=$(which mande-chaind) start --home $HOME/.mande-chain
 Restart=on-failure
 RestartSec=3
 LimitNOFILE=65535
+
 [Install]
 WantedBy=multi-user.target
 EOF
@@ -112,73 +125,5 @@ sudo systemctl enable mande-chaind
 sudo systemctl restart mande-chaind
 
 echo '=============== SETUP FINISHED ==================='
-echo -e 'Congratulations:        \e[1m\e[32mSUCCESSFUL NODE INSTALLATION\e[0m'
-echo -e 'To check logs:        \e[1m\e[33mjournalctl -u mande-chaind -f -o cat\e[0m'
-echo -e "To check sync status: \e[1m\e[35mcurl -s localhost:26657/status\e[0m"
-
-break
-;;
-"Create wallet")
-echo "_|-_|-_|-_|-_|-_|-_|"
-echo -e "      \e[1m\e[35m Your WalletName:\e[0m"
-echo "_|-_|-_|-_|-_|-_|-_|"
-read Wallet
-echo export Wallet=${Wallet} >> $HOME/.bash_profile
-source ~/.bash_profile
-mande-chaind keys add $Wallet
-echo -e "      \e[1m\e[32m!!!!!!!!!SAVE!!!!!!!!!!!!!!!!SAVE YOUR MNEMONIC PHRASE!!!!!!!!!SAVE!!!!!!!!!!!!!!!!\e[0m'"
-
-break
-;;
-"Synchronization via StateSync")
-SNAP_RPC=https://mande-testnet-rpc.jambulmerah.dev:443
-peers="a3e3e20528604b26b792055be84e3fd4de70533b@38.242.199.93:24656" 
-sed -i.bak -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.mande-chain/config/config.toml
-LATEST_HEIGHT=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.height); \
-BLOCK_HEIGHT=$((LATEST_HEIGHT - 500)); \
-TRUST_HASH=$(curl -s "$SNAP_RPC/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
-echo $LATEST_HEIGHT $BLOCK_HEIGHT $TRUST_HASH
-sed -i.bak -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \
-s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$SNAP_RPC,$SNAP_RPC\"| ; \
-s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
-s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"| ; \
-s|^(seeds[[:space:]]+=[[:space:]]+).*$|\1\"\"|" $HOME/.mande-chain/config/config.toml
-mande-chaind tendermint unsafe-reset-all --home /root/.mande-chain --keep-addr-book
-sudo systemctl restart mande-chaind && journalctl -u mande-chaind -f -o cat
-
-
-break
-;;
-"UPDATE")
-echo -e "        \e[1m\e[32mVERSION\e[0m"
-mande-chaind version
-
-break
-;;
-"Check node logs")
-journalctl -u mande-chaind -f -o cat
-
-
-break
-;;
-"Synchronization via SnapShot")
-echo -e "      \e[1m\e[32m SOOOOON  \e[0m"
-
-break
-;;
-"Delete Node")
-sudo systemctl stop mande-chaind && \
-sudo systemctl disable mande-chaind && \
-rm /etc/systemd/system/mande-chaind.service && \
-sudo systemctl daemon-reload && \
-cd $HOME && \
-rm -rf .mande-chain && \
-rm -rf $(which mande-chaind)
-
-break
-;;
-"Exit")
-exit
-esac
-done
-done
+echo -e 'To check logs: \e[1m\e[32mjournalctl -u mande-chaind -f -o cat\e[0m'
+echo -e "To check sync status: \e[1m\e[32mcurl -s localhost:${MANDE_PORT}657/status | jq .result.sync_info\e[0m"
