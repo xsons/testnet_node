0633bb3457e106eb51857315cd3ea5089cb58042
xsons <108946833+xsons@users.noreply.github.com>
2022-11-07 23:02:58 +0700

    Update massa.sh

--- massalabs/massa.sh
+++ massalabs/massa.sh
@@ -23,17 +23,17 @@ echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++"
 sleep 1
 
 # set vars
-if [ ! $IP_SERVER ]; then
-read -p "Input IP Server Kamu: " IP_SERVER
-echo 'export IP_SERVER='\"${IP_SERVER}\" >> $HOME/.bash_profile
-read -p "Input Password Kamu: " PASSWORD
+if [ ! $IP_VPS ]; then
+read -p "Input IP VPS: " IP_VPS
+echo 'export IP_VPS='\"${IP_VPS}\" >> $HOME/.bash_profile
+read -p "Password: " PASSWORD
 echo 'export PASSWORD='\"${PASSWORD}\" >> $HOME/.bash_profile
 fi
 echo 'source $HOME/.bashrc' >> $HOME/.bash_profile
 . $HOME/.bash_profile
 
-echo -e "IP Server Kamu: \e[1m\e[32m${IP_SERVER}\e[0m"
-echo -e "Password Kamu: \e[1m\e[32m${PASSWORD}\e[0m"
+echo -e "IP VPS: \e[1m\e[32m${IP_VPS}\e[0m"
+echo -e "Password: \e[1m\e[32m${PASSWORD}\e[0m"
 echo '================================================='
 sleep 1
 
@@ -67,7 +67,7 @@ tar xvzf massa_TEST.16.0_release_linux.tar.gz
 cd $HOME
 cd massa/massa-node/config && rm -rf config.toml
 wget https://raw.githubusercontent.com/mdlog/testnet-mdlog/main/massa/config.toml
-sed -i -e "s/^routable_ip *=.*/routable_ip = \"$IP_SERVER\"/" $HOME/massa/massa-node/config/config.toml
+sed -i -e "s/^routable_ip *=.*/routable_ip = \"$IP_VPS\"/" $HOME/massa/massa-node/config/config.toml
 
 sudo tee /root/massa/massa-node/run.sh > /dev/null <<EOF
 #!/bin/bash
@@ -97,20 +97,20 @@ systemctl restart massad
 
 #!/bin/bash
 if [ ! $PASSWORD ]; then
-read -p "Input Password Client Kamu: " PASSWORD
+read -p "Password: " PASSWORD
 echo 'export PASSWORD='\"${PASSWORD}\" >> $HOME/.bash_profile
 fi
 echo 'source $HOME/.bashrc' >> $HOME/.bash_profile
 . $HOME/.bash_profile
 
-echo -e "Password Client Kamu: \e[1m\e[32m${PASSWORD}\e[0m"
+echo -e "Password: \e[1m\e[32m${PASSWORD}\e[0m"
 echo '================================================='
-PASSWORDKU=$PASSWORD
+PASSWORD=$PASSWORD
 sleep 1
 # Generate New Wallet 
 
 chmod +x $HOME/massa/massa-client/massa-client
-cd $HOME/massa/massa-client && wallet_pss=$(./massa-client -p $PASSWORDKU | grep Address )
+cd $HOME/massa/massa-client && wallet_pss=$(./massa-client -p $PASSWORD | grep Address )
 cd $HOME/massa/massa-client && generate_address=$(./massa-client wallet_generate_secret_key -p $PASSWORDKU | grep Address | awk '{ print $2 }')
 sleep 30
 
