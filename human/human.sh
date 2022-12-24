f8307e2d8a2ad0455db94e9156aad524e85cfc6d
xsons <108946833+xsons@users.noreply.github.com>
2022-12-24 20:41:29 +0700

    Update human.sh

--- human/human.sh
+++ human/human.sh
@@ -47,32 +47,105 @@ echo "╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬╬�
 
 sleep 2
 
-echo -e "\e[1m\e[32m1. Install packages... \e[0m" && sleep 1
+read -p "Enter node moniker: " NODE_MONIKER
+
+CHAIN_ID="testnet-1"
+CHAIN_DENOM="uheart"
+BINARY="humansd"
+
+
+echo -e "Node moniker: ${CYAN}$NODE_MONIKER${NC}"
+echo -e "Chain id:     ${CYAN}$CHAIN_ID${NC}"
+echo -e "Chain demon:  ${CYAN}$CHAIN_DENOM${NC}"
+sleep 2
+
+echo -e "\e[1m\e[32m1. Updating packages... \e[0m" && sleep 1
 # update
-cd $HOME
 sudo apt update && sudo apt upgrade -y
-sudo apt install curl tar wget clang pkg-config libssl-dev jq build-essential bsdmainutils git make ncdu gcc git jq chrony liblz4-tool -y
-echo -e "\e[1m\e[32m1. Install Bahan Guys... \e[0m" && sleep 1
-wget https://github.com/humansdotai/humans/releases/download/latest/humans_latest_linux_amd64.tar.gz
-tar -xvf humans_latest_linux_amd64.tar.gz
-export PATH=<path-to-humansd>:$PATH
-sudo cp humansd /usr/local/bin/humansd
-echo -e "\e[1m\e[32m1. Update  packages Dulu Ya... \e[0m" && sleep 1
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt install git build-essential ufw curl jq snapd --yes
-echo -e "\e[1m\e[32m1. Install Goo Dulu... \e[0m" && sleep 1
-wget https://golang.org/dl/go1.18.2.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.18.2.linux-amd64.tar.gz
-export GOROOT=/usr/local/go
-export GOPATH=$HOME/go
-export GO111MODULE=on
-export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin
-echo -e "\e[1m\e[32m1. Dikit Lagi Say... \e[0m" && sleep 1
+
+echo -e "\e[1m\e[32m2. Installing dependencies... \e[0m" && sleep 1
+# packages
+sudo apt-get install make build-essential gcc git jq chrony screen -y
+
+# install go
+if ! [ -x "$(command -v go)" ]; then
+  ver="1.19"
+  cd $HOME
+  wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
+  sudo rm -rf /usr/local/go
+  sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
+  rm "go$ver.linux-amd64.tar.gz"
+  echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile
+  source ~/.bash_profile
+fi
+
+echo -e "\e[1m\e[32m3. Downloading and building binaries... \e[0m" && sleep 1
+# download and build binaries
 cd $HOME
+rm -rf humans
 git clone https://github.com/humansdotai/humans
 cd humans
 git checkout v1.0.0
 go build -o humansd cmd/humansd/main.go
-sudo cp humansd /usr/local/bin/humansd
-echo '--------------SUDAH----------------'
+sudo mv humansd /usr/bin/
+
+#config
+humansd config keyring-backend test
+humansd config chain-id testnet-1
+humansd config node tcp://localhost:26657
+
+
+# download genesis and addrbook
+curl -s https://rpc-testnet.humans.zone/genesis | jq -r .result.genesis > $HOME/.humans/config/genesis.json
+sha256sum $HOME/.humans/config/genesis.json # f5fef1b574a07965c005b3d7ad013b27db197f57146a12c018338d7e58a4b5cd
+wget -O $HOME/.humans/config/addrbook.json "https://raw.githubusercontent.com/vinjan23/Testnet.Guide/main/HumanAI/addrbook.json"
+
+# set peers and seeds
+SEEDS=""
+PEERS="1df6735ac39c8f07ae5db31923a0d38ec6d1372b@45.136.40.6:26656,9726b7ba17ee87006055a9b7a45293bfd7b7f0fc@45.136.40.16:26656,6e84cde074d4af8a9df59d125db3bf8d6722a787@45.136.40.18:26656,eda3e2255f3c88f97673d61d6f37b243de34e9d9@45.136.40.13:26656,4de8c8acccecc8e0bed4a218c2ef235ab68b5cf2@45.136.40.12:26656"
+sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.humans/config/config.toml
+sed -i.bak -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0uheart\"/;" ~/.humans/config/app.toml
+sed -i -e "s/^filter_peers *=.*/filter_peers = \"true\"/" $HOME/.humans/config/config.toml
+external_address=$(wget -qO- eth0.me) 
+sed -i.bak -e "s/^external_address *=.*/external_address = \"$external_address:26656\"/" $HOME/.humans/config/config.toml
+sed -i.bak -e "s/^seeds =.*/seeds = \"$seeds\"/" $HOME/.humans/config/config.toml
+sed -i 's/max_num_inbound_peers =.*/max_num_inbound_peers = 100/g' $HOME/.humans/config/config.toml
+sed -i 's/max_num_outbound_peers =.*/max_num_outbound_peers = 100/g' $HOME/.humans/config/config.toml
+
+# in case of pruning
+pruning="custom" && \
+pruning_keep_recent="100" && \
+pruning_keep_every="0" && \
+pruning_interval="10" && \
+sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" ~/.humans/config/app.toml && \
+sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" ~/.humans/config/app.toml && \
+sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" ~/.humans/config/app.toml && \
+sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" ~/.humans/config/app.toml
+
+wget -O snapshot.sh https://raw.githubusercontent.com/vinjan23/Testnet.Guide/main/HumanAI/snapshot.sh && chmod +x snapshot.sh && ./snapshot.sh
+
+echo -e "\e[1m\e[32m4. Starting service... \e[0m" && sleep 1
+# create service
+sudo tee /etc/systemd/system/humansd.service > /dev/null <<EOF
+[Unit]
+Description=humans
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$(which humansd) start
+Restart=on-failure
+RestartSec=3
+LimitNOFILE=65535
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# start service
+sudo systemctl daemon-reload && sudo systemctl enable humansd
+sudo systemctl restart humansd && sudo journalctl -u humansd -f -o cat
+
+echo '=============== SETUP FINISHED ==================='
+echo -e 'To check logs: \e[1m\e[32mjournalctl -u humansd -f -o cat\e[0m'
+echo -e "To check sync status: \e[1m\e[32mhumansd status 2>&1 | jq .SyncInfo\e[0m"
